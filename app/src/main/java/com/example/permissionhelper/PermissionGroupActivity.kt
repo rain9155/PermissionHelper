94d6803e6640ba2e37ae79907f5289d0f605f9e3
rain9155 <jianyu9155@gmail.com>
2021-09-25 01:58:24 +0800

    android 10以后手动把危险权限划分到对应的权限组

--- app/src/main/java/com/example/permissionhelper/PermissionGroupActivity.kt
+++ app/src/main/java/com/example/permissionhelper/PermissionGroupActivity.kt
@@ -1,7 +1,10 @@
 package com.example.permissionhelper
 
+import android.Manifest
+import android.annotation.TargetApi
 import android.content.pm.PermissionGroupInfo
 import android.content.pm.PermissionInfo
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,14 +26,69 @@ class PermissionGroupActivity : AppCompatActivity() {
         private const val TAG = "PermissionActivity"
     }
 
+    @TargetApi(Build.VERSION_CODES.Q)
+    private val permissionGroupToDangerousPermissions = mapOf(
+        Manifest.permission_group.CALENDAR to listOf(
+            Manifest.permission.READ_CALENDAR,
+            Manifest.permission.WRITE_CALENDAR
+        ),
+        Manifest.permission_group.CALL_LOG to listOf(
+            Manifest.permission.READ_CALL_LOG,
+            Manifest.permission.WRITE_CALL_LOG,
+            Manifest.permission.PROCESS_OUTGOING_CALLS
+        ),
+        Manifest.permission_group.CAMERA to listOf(
+            Manifest.permission.CAMERA
+        ),
+        Manifest.permission_group.CONTACTS to listOf(
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.WRITE_CONTACTS,
+            Manifest.permission.GET_ACCOUNTS
+        ),
+        Manifest.permission_group.LOCATION to listOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        ),
+        Manifest.permission_group.MICROPHONE to listOf(
+            Manifest.permission.RECORD_AUDIO
+        ),
+        Manifest.permission_group.PHONE to listOf(
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_PHONE_NUMBERS,
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.ANSWER_PHONE_CALLS,
+            Manifest.permission.ADD_VOICEMAIL,
+            Manifest.permission.USE_SIP,
+            Manifest.permission.ACCEPT_HANDOVER
+        ),
+        Manifest.permission_group.SENSORS to listOf(
+            Manifest.permission.BODY_SENSORS
+        ),
+        Manifest.permission_group.SMS to listOf(
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.RECEIVE_SMS,
+            Manifest.permission.READ_SMS,
+            Manifest.permission.RECEIVE_WAP_PUSH,
+            Manifest.permission.RECEIVE_MMS
+        ),
+        Manifest.permission_group.STORAGE to listOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_MEDIA_LOCATION
+        ),
+        Manifest.permission_group.ACTIVITY_RECOGNITION to listOf(
+            Manifest.permission.ACTIVITY_RECOGNITION
+        )
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission_group)
-        supportActionBar?.title = "权限列表"
+        supportActionBar?.title = "权限组和权限列表"
 
         val permissionGroups = this.packageManager.getAllPermissionGroups(0)
-        val permissions = ArrayList<List<PermissionInfo>>(permissionGroups.size)
+        val permissions = ArrayList<MutableList<PermissionInfo>>(permissionGroups.size)
         permissionGroups.forEach { permissionGroup ->
             permissions.add(this.packageManager.queryPermissionsByGroup(permissionGroup.name, 0))
         }
@@ -39,13 +97,23 @@ class PermissionGroupActivity : AppCompatActivity() {
             permissionGroup.name.contains(UNDEFINED)
         }
         if(undefinedPermissionGroup != null){
+            //undefinedPermission移动到列表顶部
             val undefinedPermissions = permissions[permissionGroups.indexOf(undefinedPermissionGroup)]
             permissionGroups.remove(undefinedPermissionGroup)
             permissionGroups.add(0, undefinedPermissionGroup)
             permissions.remove(undefinedPermissions)
             permissions.add(0, undefinedPermissions)
+            //android 10以后手动把危险权限划分到对应的权限组
+            permissionGroups.forEachIndexed { index, permissionGroup ->
+                if(permissionGroupToDangerousPermissions.containsKey(permissionGroup.name)) {
+                    val dangerousPermissions = permissionGroupToDangerousPermissions[permissionGroup.name]
+                    val dangerousPermissionInfos = undefinedPermissions.filter { permissionInfo ->
+                        dangerousPermissions?.contains(permissionInfo.name) ?: false
+                    }
+                    permissions[index].addAll(dangerousPermissionInfos)
+                }
+            }
         }
-
 
         Log.d(TAG, "permissionGroups = $permissionGroups, permissions = $permissions")
 
